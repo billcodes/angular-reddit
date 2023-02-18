e332eadfc87554bfef5ba40bcb312f56e8dfad6c
bill <bill@veniosystems.com>
2023-02-18 11:32:52 +0545

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   //title = 'angular-reddit';
+  articles: Article[];
+  constructor()
+  {
+    this.articles=[
+      new Article("Angular2","http://angular.com/test1",0),
+      new Article("Fullstack","http://fullstack.com/test2",0),
+      new Article("AngularHomePage","http://angular.com/test3",0),
+    ];
+  }
+  addArticle(title:HTMLInputElement,link:HTMLInputElement){
+    this.articles.push(new Article(title.value,link.value,0));
+    return false;
+  }
+  sortedArticles():Article[]{
+    return this.articles.sort((a:Article,b:Article)=>b.votes-a.votes);
+  }
 }
